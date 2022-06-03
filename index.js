721c8ad5889f083e45f16f893be5befba9c0ea20
arif nur rahman <arnu12rahman@gmail.com>
2022-06-03 08:25:23 +0700

    08 - Challenge Advanced Function

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+    "2019-01-02", // 1546387200 (epoch time, dalam detik)
+    "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
+    "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
+    "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
+    "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (dates, index = -1) => {
+    const timeToSecond = dates.map((item) => new Date(item).getTime() / 1000); //create array baru dengan content hasil convert ke second dates
+    const result = index == -1 ? timeToSecond : timeToSecond.splice(index, 1); // cek apakah index default or tidak kalau tidak get data sesuai pilihan index
+
+    //return result in sort function
+    return result.sort((a, b) => a - b).join("-");
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
-  // IIFE
+    // IIFE
 
-  // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
-  console.log(createDate?.(dates));
+    // '1546387200-1580662800-1614841200-1617573600-1651802400' (dalam string)
+    console.log(createDate?.(dates));
 
-  // '1614841200' (dalam string)
-  console.log(createDate?.(dates, 2));
+    // '1614841200' (dalam string)
+    console.log(createDate?.(dates, 2));
 })();
 
 module.exports = {
-  dates,
-  createDate,
+    dates,
+    createDate,
 };
